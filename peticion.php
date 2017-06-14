6e21e8c093535eff5dae9ae806e39a2a13f12f92
alfredosolisb <alfredito_sol@hotmail.com>
2017-06-14 10:02:52 -0500

    SOBA_DSW32
    Alfredo Alejandro Solis Burgos TI32

---
+++ peticion.php
@@ -0,0 +1,14 @@
+<<<<<<< HEAD
+< ?php
+   echo '<p id="nombre">'.$_GET['nombre']."</p>";
+   echo '<p id="edad">'.$_GET['edad']."</p>"; 
+? >
+
+
+=======
+<?php
+echo '<p id="nombre">'.$_GET['nombre']."</p>";
+echo '<p id="edad">'.$_GET['edad']."</p>";
+
+?>
+>>>>>>> 19e62ea016b6dd72b12b8eab1472faf2bed13c45
